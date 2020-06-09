574d89b249f2dab199e13125fdb9bfe62e10652f
Banny Dai <cssf998811@gmail.com>
2020-06-09 23:22:49 +0800

    login

--- action.php
+++ action.php
@@ -3,24 +3,24 @@ session_start();
 $action = $_POST["action"];
 
 if ($action == "login") {
-    $name1 = $_POST["account"];
-    $pass1 = $_POST["password"];
+    $name1 = $_POST["username"];
+    $pass1 = $_POST["userpass"];
 
     $servername = "localhost";
-    $dbname   = "school";
-    $username = "school";
-    $password = "abc123";
+    $username = "cssf998811";
+    $password = "cssf118899";
+    $dbname = "dindin";
     $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
 
     // set the PDO error mode to exception
     $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $conn->prepare("SELECT * FROM `users` WHERE `account`='$name1' AND `password`='$pass1';");
+    $stmt = $conn->prepare("SELECT * FROM `user_data` WHERE `account`='$name1' AND `password`='$pass1';");
     $stmt->execute();
 
     //帳密正確
     if ($stmt->rowCount()==1) {
-        $_SESSION["account"] = $name1;
+        $_SESSION["username"] = $name1;
         echo "Yes";
     } else {
         echo "No";
@@ -28,7 +28,7 @@ if ($action == "login") {
 
 } elseif ($action == "logout") {
 
-    unset($_SESSION["account"]);
+    unset($_SESSION["username"]);
     session_destroy();  
 
 }
